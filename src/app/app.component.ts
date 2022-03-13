beec92e985741d53f0d507fb7ddafc79189849f2
matheustavares <matheustavares1994@gmail.com>
2022-03-13 16:31:26 +0100

    aggiunta mappa e trova posizione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { styles } from './mapstyles';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chelaTest';
+export class AppComponent implements OnInit {
+
+  title = 'google-maps';
+
+  private map: google.maps.Map | undefined;
+  private infoWindow: google.maps.InfoWindow | undefined;
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyBk7L-BBBVifb2z_XpVNLXY4BsGig15TOg'
+    })
+
+    loader.load().then(() => {
+      console.log('loaded gmaps')
+
+      const location = { lat: 51.233334, lng: 	6.783333 }
+
+      let htmlMap = document.getElementById("map");
+      if(htmlMap != null){
+        this.infoWindow = new google.maps.InfoWindow();
+        this.map = new google.maps.Map(htmlMap, {
+          center: location,
+          zoom: 6,
+          styles: styles
+        });
+
+        const marker = new google.maps.Marker({
+          position: location,
+          map: this.map,
+        });
+      }
+    })
+  }
+  /**
+   * activate geolocation and set the pin on the current location
+   * @returns 
+   */
+  findMyLocation(): void{
+    console.log("sono qui");
+    if(this.infoWindow === undefined )
+      return;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          this.infoWindow?.setPosition(pos);
+          this.infoWindow?.open(this.map);
+          this.map?.setCenter(pos);
+          this.createPin(pos);
+        },
+        () => {
+          if(this.infoWindow != undefined)
+            this.handleLocationError(true, this.infoWindow, this.map?.getCenter()!);
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      this.handleLocationError(false, this.infoWindow, this.map?.getCenter()!);
+    }
+  }
+
+  /**
+   * create a pin on the map base on the input position
+   * @param customPosition position for create the pin on the map
+   */
+  createPin(customPosition: google.maps.MarkerOptions["position"]): void{
+    const marker = new google.maps.Marker({
+      position: customPosition,
+      map: this.map,
+    });
+
+  }
+  
+  handleLocationError(
+    browserHasGeolocation: boolean,
+    infoWindow: google.maps.InfoWindow,
+    pos: google.maps.LatLng
+  ) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(this.map);
+  }
+  
 }
